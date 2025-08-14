0fd62c43245fc0ad8dee10d540ea880acc3814aa
jussarag-cell <jussara.g@escolar.ifrn.edu.br>
2025-08-14 20:49:33 +0000

    Desafio de programação - 04

---
+++ src/Components/Hearder.jsx
@@ -0,0 +1,18 @@
+import styles from "./Header.module.css";
+import img from "../assets/img/Brasaomacau.png";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.centralContainer}>
+        <div className={styles.containerImg}>
+          {/* <img src={img}
+          alt="Macau"
+          /> */}
+        </div>
+        {/* <h1 className={styles.tema}>Macau - Cidade do Sal</h1> */}
+        <h1 className={styles.tema}>Header</h1>
+      </div>
+    </header>
+  );
+}
