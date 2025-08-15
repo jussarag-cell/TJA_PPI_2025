aee6e73062151e442def3c65ab12f4af0cd02aac
jussarag-cell <jussara.g@escolar.ifrn.edu.br>
2025-08-15 14:51:24 +0000

    Desafio_07

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { ProductList } from "./components/ProductList";
-import { Header } from "./components/Header";
-import { useState } from "react";
+// import { ProductList } from "./components/ProductList";
+// import { Header } from "./components/Header";
+// import { Route, Routes } from "react-router";
+// import { Cart } from "./components/Cart";
+// import { CartProvider } from "./service/CartContext";
+import CadastrarUsuarios  from "./components/CadastrarUsuarios";
+import CadastrarProdutos from "./components/CadastrarProdutos";
+import FazerLogin from "./components/FazerLogin";
+
 
 export default function App() {
-
-  const [cart, setCart] = useState([]);
-
-  function addToCart(product) {
-    setCart((prevCart) => [...prevCart, product]);
-  }
 
   return (
     //React Fragment
     <>
-
-      <Header cart={cart} />
-      <ProductList addToCart={addToCart} />
+    <FazerLogin />
+    <CadastrarUsuarios />
+    {/* <CartProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/produtos" element={<TelaProdutos />} />
+      </Routes>
+    </CartProvider> */}
+    <CadastrarProdutos />
     </>
   );
 }
